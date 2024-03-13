06a339dac3faff66e438ad94e9335f2c8ea3f4c1
NajlaF <najlafahad011@gmail.com>
2024-03-13 23:14:36 +0300

    Initial commit

---
+++ src/app.py
@@ -0,0 +1,38 @@
+'''
+ # @ Create Time: 2024-03-13 12:12:32.711699
+'''
+
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__, title="retest")
+
+# Declare server for Heroku deployment. Needed for Procfile.
+server = app.server
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
